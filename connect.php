eb95a2b15b4a1b73fd72b893faa11e7d3d1150ac
Ashwin-ER <143249415+Ashwin-ER@users.noreply.github.com>
2023-08-27 23:43:39 +0530

    connect.php

---
+++ connect.php
@@ -0,0 +1,23 @@
+<?php
+	/*$name = $_POST['name'];
+	$Email = $_POST['Email'];
+	$phoneno = $_POST['phoneno'];
+	$address = $_POST['address'];
+	$card = $_POST['card'];
+
+	// Database connection
+	$conn = new mysqli('localhost','root','','mini');
+	if($conn->connect_error){
+		die("Connection Failed : ". $conn->connect_error);
+		echo "$conn->connect_error";
+	}
+	 else {
+		$stmt = $conn->prepare("insert into store(name, Email, phoneno, address, card) values(?, ?, ?, ?, ?)");               
+		$stmt->bind_param("si, $name, $Email, $phoneno, $address, $card);
+		$stmt->execute();
+		echo "Registration successfully...";
+		$stmt->close();
+		$conn->close();
+	}*/
+    echo "vhjhhihb";
+?>
